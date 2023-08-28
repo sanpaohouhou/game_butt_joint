2a5f5ed8b91fb2deb9dd56499f13bfacf0a0cb0b
wyx <tomwang916@163.com>
2023-08-28 09:15:43 +0800

    fix2

--- src/main/java/com/tl/tgGame/project/service/CommissionPennyService.java
+++ src/main/java/com/tl/tgGame/project/service/CommissionPennyService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommissionPennyService {
 
+
     @Autowired
     private CurrencyService currencyService;
 
