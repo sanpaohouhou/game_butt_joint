b54e5a5ebe8f37cca17e62a9f8ccb4f6ba484d9b
wyx <tomwang916@163.com>
2023-09-01 15:09:46 +0800

    fix

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ mvn clean install -DskipTests
 
 ps -ef |grep java |grep ROOT  |grep -v 'grep'|awk '{print $2}'  | xargs kill -9
 
-export spring_datasource_url='jdbc:mysql://development.ceezehox1xag.ap-northeast-1.rds.amazonaws.com:3306/baccarat?characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai'
+export spring_datasource_url='jdbc:mysql://development.ceezehox1xag.ap-northeast-1.rds.amazonaws.com:3306/game_butt_joint?characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai'
 export spring_datasource_username='admin'
 export spring_datasource_password='tianli123456TL'
 export spring_redis_host='development.hej4hw.ng.0001.apne1.cache.amazonaws.com'
 export spring_redis_database='9'
 
 
-nohup java -jar target/ROOT.jar &
+nohup java -jar target/ROOT.jar > logfile.log &
