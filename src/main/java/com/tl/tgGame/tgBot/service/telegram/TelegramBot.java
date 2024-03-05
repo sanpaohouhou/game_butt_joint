79433d91d0422f02c43e26a89ba021de1e31104f
sanpaohouhou <tomwang916@163.com>
2024-03-05 11:50:48 +0800

    8

--- src/main/java/com/tl/tgGame/tgBot/service/telegram/TelegramBot.java
+++ src/main/java/com/tl/tgGame/tgBot/service/telegram/TelegramBot.java
@@ -150,9 +150,9 @@ public class TelegramBot extends TelegramLongPollingBot {
                 String decrypt = AESUtil.encrypt(String.valueOf(user.getId()), securityKey);
                 String h5Url = configService.get(ConfigConstants.BOT_TG_GAME_H5_URL);
                 AnswerCallbackQuery answerCallbackQuery = new AnswerCallbackQuery();
-//                answerCallbackQuery.setUrl(h5Url + "?token=" + decrypt + "&type=" + "FCBY");
-                answerCallbackQuery.setUrl(h5Url + "?token=" + decrypt + "&type=" + GameBusiness.FC.getKey());
-                answerCallbackQuery.setCallbackQueryId(callbackQuery.getId());
+                answerCallbackQuery.setUrl(h5Url + "?token=" + decrypt + "&type=" + "FCBY");
+//                answerCallbackQuery.setUrl(h5Url + "?token=" + decrypt + "&type=" + GameBusiness.FC.getKey());
+//                answerCallbackQuery.setCallbackQueryId(callbackQuery.getId());
                 execute(answerCallbackQuery);
             }
             if (callbackQuery.getGameShortName() != null && callbackQuery.getGameShortName().equals("EG_GAME")) {
@@ -222,7 +222,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                 String decrypt = AESUtil.encrypt(String.valueOf(user.getId()), securityKey);
                 String h5Url = configService.get(ConfigConstants.BOT_TG_GAME_H5_URL);
                 AnswerCallbackQuery answerCallbackQuery = new AnswerCallbackQuery();
-                answerCallbackQuery.setUrl(h5Url + "/agland?token=" + decrypt + "&type=" + GameBusiness.AG.getKey());
+                answerCallbackQuery.setUrl(h5Url + "/agland?token=" + decrypt + "&type=" + GameBusiness.JDB.getKey());
                 answerCallbackQuery.setCallbackQueryId(callbackQuery.getId());
                 execute(answerCallbackQuery);
             }
